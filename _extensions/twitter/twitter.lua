bbd33d28e8cceeb28841097f9a68d3821eb52706
Mark Sellors <github@5vcc.com>
2022-08-09 20:24:50 +0100

    Adds support for multiple embedded tweets

--- _extensions/twitter/twitter.lua
+++ _extensions/twitter/twitter.lua
@@ -2,7 +2,8 @@ function tweet(args)
     local url = pandoc.utils.stringify(args[1])
     local tweet_embed = 'https://publish.twitter.com/oembed?url=' .. url .. '&align=center'
     local mt, api_resp = pandoc.mediabag.fetch(tweet_embed)
-    local tweet_data = '<div id="tweet"></div><script>tweet=' .. api_resp .. ';document.getElementById("tweet").innerHTML = tweet["html"];</script>'
+    local id = math.random(10000, 99999)
+    local tweet_data = '<div id="tweet-' .. id .. '"></div><script>tweet=' .. api_resp .. ';document.getElementById("tweet-' .. id .. '").innerHTML = tweet["html"];</script>'
     if quarto.doc.isFormat('html') then
         quarto.doc.addHtmlDependency({
             name = "twitter",
